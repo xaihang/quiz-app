82d091171a8430351f39a111c16705151ce493bf
xaihang <xaihang12@gmail.com>
2022-10-26 10:06:35 -0500

    restQuiz btn function added

--- script.js
+++ script.js
@@ -41,4 +41,25 @@ let counter;
 let counterLine;
 let widthValue = 0;
 
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = () => {
+    quiz_box.classList.add("activeQuiz");
+    result_box.classList.remove("activeResult");
+    timeValue = 15;
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    clearInterval(counterLine);
+    startTimer(timeValue);
+    startTimerLine(widthValue);
+    timeText.textContent = "Time Left";
+    next_btn.classList.remove("show");
+}
 
