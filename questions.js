0b4cc95aef2df28f27bd44801483f77038d2c63e
xaihang <xaihang12@gmail.com>
2022-10-25 22:41:43 -0500

    styles added

--- questions.js
+++ questions.js
@@ -1,18 +1,18 @@
 //Questions and Options 
 
-1. What are boolean operators ?
-    a. && (And)
-b. || (Or)
-c. !(Not)
-d.All the above
-ANSWER: "d", "All the above"
+// 1. What are boolean operators ?
+//     a. && (And)
+// b. || (Or)
+// c. !(Not)
+// d.All the above
+// ANSWER: "d", "All the above"
 
-2. What are recognized errors in JavaScript ?
-    a.Load - time errors
-b.Runtime errors
-c.Logics errors
-d.All the above
-ANSWER: "d", "All the above"
+// 2. What are recognized errors in JavaScript ?
+//     a.Load - time errors
+// b.Runtime errors
+// c.Logics errors
+// d.All the above
+// ANSWER: "d", "All the above"
 
 3. What does the following statement declare ?
 var myArray = [[[]]];
@@ -20,7 +20,7 @@ a.The array object
 b.A three - dimensional array
 c.Empty string of data stored in array
 d.Square brackets
-ANSWER: "b", "It declares a three-dimensional array"
+ANSWER: "b", "A three - dimensional array"
 
 4. What are the four type of loops in JavaScript ?
     a.for loop, for/in loop, while loop, and do while loop
@@ -35,3 +35,63 @@ b.Adding new properties and methods
 c.Adding a value as the last item of the array
 d.Removing the first item of an array
 ANSWER: "c", "Adding a value as the last item of the array"
+
+{
+    numb: 1,
+        question: "What are boolean operators?",
+            answer: "All the above",
+                options: [
+                    "&& (And)",
+                    "|| (Or)",
+                    "!(Not)",
+                    "All the above"
+                ]
+},
+
+{
+    numb: 2,
+        question: "What does the following statement declare? var myArray = [[[]]];",
+            answer: "A three - dimensional array",
+                options: [
+                    "The array object",
+                    "A three - dimensional array",
+                    "Empty string of data stored in array",
+                    "Square brackets"
+                ]
+},
+
+{
+    numb: 3,
+        question: "What are recognized errors in JavaScript?",
+            answer: "All the above",
+                options: [
+                    "Load - time errors",
+                    "Runtime errors",
+                    "Logics errors",
+                    "All the above"
+                ]
+},
+
+{
+    numb: 4,
+        question: "Your Question is Here",
+            answer: "Correct answer of the question is here",
+                options: [
+                    "Option 1",
+                    "option 2",
+                    "option 3",
+                    "option 4"
+                ]
+},
+
+{
+    numb: 5,
+        question: "Your Question is Here",
+            answer: "Correct answer of the question is here",
+                options: [
+                    "Option 1",
+                    "option 2",
+                    "option 3",
+                    "option 4"
+                ]
+},
