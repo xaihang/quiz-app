c3ca8d52942771a156e3a33a18a097b6b722c24f
xaihang <xaihang12@gmail.com>
2022-10-25 21:49:36 -0500

    new js file with questions

---
+++ questions.js
@@ -0,0 +1,35 @@
+1. What are boolean operators?
+a. && (And)
+b. || (Or)
+c. ! (Not)
+d. All the above
+ANSWER: "d", "All the above"
+
+2. What are recognized errors in JavaScript?
+a. Load-time errors
+b. Runtime errors
+c. Logics errors
+d. All the above
+ANSWER: "d", "All the above"
+
+3. What does the following statement declare?
+var myArray = [[[]]];
+a. The array object
+b. A three-dimensional array
+c. Empty string of data stored in array
+d. Square brackets
+ANSWER: "b", "It declares a three-dimensional array"
+
+4. What are the four type of loops in JavaScript?
+a. for loop, for/in loop, while loop, and do while loop
+b. for loop, for each loop, for of loop, and while loop
+c. while loop, loop, function of, and into loop
+d. None of the above
+ANSWER: "a", "for loop, for/in loop, while loop, and do while loop"
+
+5. What is a push method?
+a. The number of elements in an array
+b. Adding new properties and methods
+c. Adding a value as the last item of the array
+d. Removing the first item of an array
+ANSWER: "c", "Adding a value as the last item of the array"
