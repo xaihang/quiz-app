afb524dc5a5ce77a644be2d91f9b98afd44547a8
xaihang <xaihang12@gmail.com>
2022-10-26 09:48:11 -0500

    cont btn function added

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ exit_btn.onclick = () => {
     info_box.classList.remove("activeInfo");
 }
 
+// if continueQuiz button clicked
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
+    queCounter(1); //passing 1 parameter to queCounter
+    startTimer(25); //calling startTimer function
+    startTimerLine(0); //calling startTimerLine function
+}
+
